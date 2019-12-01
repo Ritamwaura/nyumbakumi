fee003abd38294e04fb080a4e96f5e7d48cdf8e8
Your Name <mwaurarita2019@gmail.com>
2019-12-01 23:01:52 +0300

    email file send message to new user

---
+++ hoodapp/emails.py
@@ -0,0 +1,14 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+#from .emails import send_welcome_email
+
+def send_welcome_email(name,receiver):
+    subject = 'Welcome to Nyumbakumi'
+    sender = 'mwaurarita@gmail.com'
+
+    text_content = render_to_string('emails/email.txt',{"name":name})
+    html_content = render_to_string('emails/email.html',{"name":name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
